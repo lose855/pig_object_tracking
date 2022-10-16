b86edb33885fccfde60ed60ed20328e1910da660
elon main <eloncyw@outlook.com>
2022-10-16 11:14:33 +0900

    Annotation update

---
+++ Layouts.py
@@ -0,0 +1,39 @@
+import pygame
+class Layout:
+    def __init__(self):
+        x,y = 0,1 # Position index
+        #Basis
+        self.fps = 30
+        self.width = 1240
+        self.height = 720
+        self.resize = 640
+        self.edit1Idx = 1
+        self.edit2Idx = 2
+
+        #Contorls
+        #Mousecode
+        self.leftButton = 1
+        self.centerButton = 2
+        self.backWheel = 5
+
+        #Colors
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.red = (255, 0, 0)
+
+        #Apps
+        self.edit1Pos = ((900, 110), (1150, 150))  # x1,y1 x2,y2
+        self.edit2Pos = ((955, 160), (1100, 200))  # x1,y1 x2,y2
+        self.font = pygame.font.SysFont('arial', 50, True, True)
+
+        self.label1 = [700, 40] # Image label pos
+        self.label2 = [694, 90] # Isoverlap label pos
+        self.label3 = [688, 140] # Numoverlap label pos
+        self.label4 = [687, 250] # Positions label pos
+
+        self.edit1 = [920, 90] # IsOverlapVal edit pos
+        self.edit2 = [974, 145] # OverlapNumval edit pos
+        self.edit3 = [974, 300]  # Position edit pos
+
+        self.cursorEdit1 = ((self.edit1[x] - 10, self.edit1[y]+ 25 * 1), (self.edit1[x] - 10, self.edit1[y] + 30 * 2))
+        self.cursorEdit2 = ((self.edit2[x] - 10, self.edit2[y] + 25 * 1), (self.edit2[x] - 10, self.edit2[y] + 30 * 2))
--- MultiTargetDnn.py
+++ MultiTargetDnn.py
@@ -1,12 +1,14 @@
 import torch
 import torch.nn as nn
 from PIL import Image
+from Layouts import Layout
 import sys
 import pygame
 from pygame.locals import *
 from torchvision import transforms
 import os
 import numpy as np
+import time
 import random
 import torch.optim as optim
 import keyboard
@@ -47,14 +49,9 @@ class Annotator: # Make dataset by hand
     def __init__(self, type):
         print("Type : %s is selected"%(type))
         pygame.init()
+        self.layout = Layout()
         self.type = type
-        self.fps = 30
-        self.black = (0, 0, 0)
-        self.white = (255, 255, 255)
-        self.width = 1240
-        self.height = 720
-        self.resize = 640
-        self.font = pygame.font.SysFont('arial', 20, True, True)
+        self.focusNow = -1 # default -1
         self.index = 0 # Image page
         self.framePerSec = pygame.time.Clock()
         self.inputPath = './'+type
@@ -65,55 +62,108 @@ class Annotator: # Make dataset by hand
             os.mkdir(self.resultPath)
         self.run()
 
+    class Coordinate:
+        def __init__(self, pos, scaleRate):
+            self.x = pos[0]
+            self.y = pos[1]
+            self.realX = pos[0] // scaleRate[1]
+            self.realY = pos[1] // scaleRate[0]
+        def transform(self):
+            return (self.realX, self.realY)
+        def pos(self):
+            return (self.x, self.y)
+
+
     def run(self):
-        display = pygame.display.set_mode((self.width, self.height))
-        display.fill(self.black)
+        # edit1 : isOverlapping True/False
+        # edit2 : overlappingNumber Integer
+        display = pygame.display.set_mode((self.layout.width, self.layout.height))
         pygame.display.set_caption(self.type)
-        isOverlapping = self.font.render("Is overlapping: ", True, self.white)
-        numOverlapping = self.font.render("Num overlapping: ", True, self.white)
-        overlappingRect = isOverlapping.get_rect()
-        overlappingRect.topleft = (20, 20)
-        overlappingCursor = Rect(overlappingRect.topright, (3, overlappingRect.height))
+        isOverlapVal = self.type
+        overlapNumval = "0"
+        edit1 = self.layout.font.render(isOverlapVal, True, self.layout.white)
+        edit2 = self.layout.font.render(overlapNumval, True, self.layout.white)
+        coordList = []
+
         while True:
-            display.fill(self.black)
+            display.fill(self.layout.black)
             image = self.images[self.index]
             image = pygame.image.load(self.inputPath+'/'+image)
             imageWidth = image.get_width()
             imageHeight = image.get_height()
-            image = pygame.transform.scale(image, (self.resize, self.resize))
-            scaleRate = (self.resize/imageHeight, self.resize/imageWidth) # HxW
-            display.blit(image, [50, 40])
-            imageNumber = self.font.render("Image: %d"%(self.index), True, self.white)
-            # coordinate = self.font.render("Coordinate-%d : X: %d Y: %d ", True, self.white)
-            display.blit(imageNumber, [700, 40])
-            display.blit(isOverlapping, [700, 80])
+            image = pygame.transform.scale(image, (self.layout.resize, self.layout.resize))
+            scaleRate = (self.layout.resize/imageHeight, self.layout.resize/imageWidth) # HxW
+            display.blit(image, [30, 40])
+            imageNumber = self.layout.font.render("Image: %d"%(self.index), True, self.layout.white)
+        
+            isOverlapping = self.layout.font.render("Is overlap:", True, self.layout.white)
+            numOverlapping = self.layout.font.render("Num overlap:", True, self.layout.white)
+            positions = self.layout.font.render("Positions:", True, self.layout.white)
+            edit1 = self.layout.font.render(isOverlapVal, True, self.layout.white)
+            edit2 = self.layout.font.render(overlapNumval, True, self.layout.white)
 
-            display.blit(numOverlapping, [700, 120])
-            for event in pygame.event.get():
+            display.blit(imageNumber, self.layout.label1)
+            display.blit(isOverlapping, self.layout.label2)
+            display.blit(numOverlapping, self.layout.label3)
+            display.blit(positions, self.layout.label4)
+            display.blit(edit1, self.layout.edit1)
+            display.blit(edit2, self.layout.edit2)
+            
+            for event in pygame.event.get(): # Evnet handler
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
+
                 if event.type == pygame.KEYDOWN:
                     if event.key == K_BACKSPACE:
-                        if len(text) > 0:
-                            text = text[:-1]
+                        if self.focusNow == self.layout.edit1Idx:
+                            if len(isOverlapVal) > 0:
+                                isOverlapVal = isOverlapVal[:-1]
+                        elif self.focusNow == self.layout.edit2Idx:
+                            if len(overlapNumval) > 0:
+                                overlapNumval = overlapNumval[:-1]
+                    elif event.key == pygame.K_RIGHT:
+                        self.index = (self.index+1) % self.totalImages
+                    elif event.key == pygame.K_LEFT:
+                        if not self.index == 0:
+                            self.index -= 1
                     else:
-                        text += event.unicode
-                    if event.key == pygame.K_RIGHT:
-                        self.index = (self.index+1) % self.totalImages
-                    if event.key == pygame.K_LEFT:
-                        if not self.index == 0:
-                            self.index -= 1
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 2: # Center button
-                        self.index = (self.index+1) % self.totalImages
-                        pygame.time.delay(70)
-                    if event.button == 5: # Back wheel
-                        if not self.index == 0:
-                            self.index -= 1
+                        if self.focusNow == self.layout.edit1Idx:
+                            isOverlapVal += event.unicode
+                        elif self.focusNow == self.layout.edit2Idx:
+                            overlapNumval += event.unicode
 
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == self.layout.leftButton:  # Left button
+                        pos = event.pos
+                        if self.layout.edit1Pos[0][0]<=pos[0]<=self.layout.edit1Pos[1][0] and self.layout.edit1Pos[0][1]<=pos[1]<=self.layout.edit1Pos[1][1]:
+                            self.focusNow = self.layout.edit1Idx
+                            cursor = self.layout.cursorEdit1
+                        elif self.layout.edit2Pos[0][0]<=pos[0]<=self.layout.edit2Pos[1][0] and self.layout.edit2Pos[0][1]<=pos[1]<=self.layout.edit2Pos[1][1]:
+                            self.focusNow = self.layout.edit2Idx
+                            cursor = self.layout.cursorEdit2
+                        else:
+                            self.focusNow = -1
+                        if 30<=pos[0]<=self.layout.resize+30 and 40<=pos[1]<=self.layout.resize+40:
+                            coordList.append(self.Coordinate(pos, scaleRate))
+                    if event.button == self.layout.centerButton: # Center button
+                        self.index = (self.index+1) % self.totalImages
+                        coordList = []
+                        print(len(coordList))
+                        pygame.time.delay(70)
+                    if event.button == self.layout.backWheel: # Back wheel
+                        if not self.index == 0:
+                            self.index -= 1
+                            coordList = []
+
+            if time.time() % 1 > 0.5:
+                if not self.focusNow == -1:
+                    pygame.draw.line(display, self.layout.white, cursor[0], cursor[1])
+            if not len(coordList) == 0:
+                for coordinate in coordList:
+                    pygame.draw.circle(display, self.layout.red, coordinate.pos(), 10, 3)
             pygame.display.flip() # Display update
-            self.framePerSec.tick(self.fps)
+            self.framePerSec.tick(self.layout.fps)
 
 
 
